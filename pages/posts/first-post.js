bd0758c23115080eb8b7e851a1e9dce9dc7175b0
starguys <kjj9500@naver.com>
2023-09-10 15:05:07 +0900

    learn asset, metadata, css

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -2,11 +2,11 @@ import Link from "next/link";
 import Image from "next/image";
 import Head from "next/head";
 import Script from "next/script";
-import Layout from "../../compoents/layout";
+import Layout from "../../components/layout";
 
 export default function FirstPost() {
   return (
-    <Layout>
+    <Layout posts>
       <Head>
         <title>First Post</title>
         <Script
